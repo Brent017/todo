34b47ea7474284cc423bca9186d43a97ff58796b
Brent Ottke <marieottke@Maries-MacBook-Air.local>
2019-10-24 13:00:08 -0600

    ItemList added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React from "react";
 import TodoItem from "./TodoItem";
+import todoList from "./TodoList";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todoList
+    };
+  }
+
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      });
+      return {
+        todos: updatedTodos
+      };
+    });
+  }
+
   render() {
-    return (
-      <div>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-      </div>
-    );
+    const todoItems = this.state.todos.map(item => (
+      <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
+    ));
+
+    return <div className="todo-list">{todoItems}</div>;
   }
 }
 
---
+++ src/TodoList.js
@@ -0,0 +1,24 @@
+const todoList = [
+  {
+    id: 1,
+    text: "Mow lawn",
+    completed: false
+  },
+  {
+    id: 2,
+    text: "Trim trees",
+    completed: false
+  },
+  {
+    id: 3,
+    text: "Rake leaves",
+    completed: false
+  },
+  {
+    id: 4,
+    text: "Shovel snow",
+    completed: false
+  }
+];
+
+export default todoList;
